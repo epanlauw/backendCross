cccd593b49d27cd8650c165570d79dd50f315bb0
epanlauw <epanlauw6@gmail.com>
2020-12-15 22:02:16 +0700

    Recipe crud bug fix

--- app/Recipe.php
+++ app/Recipe.php
@@ -9,6 +9,6 @@ class Recipe extends Model
     protected $table = 'recipe';
 
     protected $fillable = [
-        'id_type', 'name', 'difficulty', 'image_url', 'steps', 'id_user'
+        'id_type', 'name', 'difficulty', 'image_url', 'steps', 'id_user', 'ingredient'
     ];
 }
